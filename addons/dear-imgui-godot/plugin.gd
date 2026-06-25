e52face6de94e6720d395ccee1b14beee28fe18a
shatadev <shatadev@proton.me>
2026-06-25 09:54:38 -0700

    fix inferred type error

--- addons/dear-imgui-godot/plugin.gd
+++ addons/dear-imgui-godot/plugin.gd
@@ -1,11 +1,11 @@
 @tool
 extends EditorPlugin
 
-const AUTOLOAD_NAME := "ImGui"
+const AUTOLOAD_NAME: String = "ImGui"
 
 func _enter_tree() -> void:
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
-		var scene := get_script().resource_path.get_base_dir().path_join("imgui_api.tscn")
+		var scene: String = get_script().resource_path.get_base_dir().path_join("imgui_api.tscn")
 		add_autoload_singleton(AUTOLOAD_NAME, scene)
 
 func _exit_tree() -> void:
